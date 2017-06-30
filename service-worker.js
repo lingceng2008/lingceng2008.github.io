0a05423178eac9265440ba0d35084f38b8f6bdc3
lingceng2008 <123761423@qq.com>
2017-06-30 23:24:46 +0000

    Add index

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+var domain = "https://images.dmzj.com";
+console.log("in service worker.js");
+self.addEventListener('fetch', function (e) {
+ console.log("when fetch");
+ if (e.request.url.indexOf(domain) >= 0) {
+   var req = e.request.clone();
+   var returnUrl = req.url;
+   e.respondWith(
+     fetch(returnUrl, {
+       // 通过设置referrer为空，骗过利用referrer实现防盗链的网站
+       referrer: "http://www.dmzj.com/info/yitiaogou.html",
+       mode: 'no-cors'
+     })
+   );
+ }
+});
